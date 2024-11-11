b53bf71d90a8d396575ff56766aa313ecf8d7d39
Dario Jularic <dariojularic0@gmail.com>
2024-11-11 19:43:29 +0100

    Refactor code

--- src/GameOver.jsx
+++ src/GameOver.jsx
@@ -1,10 +1,11 @@
 import PropTypes from "prop-types";
 import "./GameOver.css";
+import ReactDom from "react-dom";
 
 function GameOver(props) {
   const { numOfCorrectAnswers, numOfQuestions } = props;
 
-  return (
+  return ReactDom.createPortal(
     <div className="game-over-container">
       {console.log("agagagagaaga")}
       <h3 className="game-over-header">Game Over</h3>
@@ -12,7 +13,8 @@ function GameOver(props) {
         You answered {numOfCorrectAnswers} / {numOfQuestions} or{" "}
         {(numOfCorrectAnswers / numOfQuestions) * 100}%{" "}
       </p>
-    </div>
+    </div>,
+    document.getElementById("root")
   );
 }
 
